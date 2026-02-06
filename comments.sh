77719d37b5058572c612c5c64a910ed8e6a8c2e2
satyam kumar <167162735+satyamkumar1507@users.noreply.github.com>
2026-02-06 23:12:42 +0530

    Update comments.sh

--- comments.sh
+++ comments.sh
@@ -3,7 +3,7 @@
 # below is the example of multi line comment
 
 <<COMMENTS
-ye jo gareeb ho be na apni aadat se garib hobe
+ye jo gareeb ho be na apni aadat se garib hobe !!!
 COMMENTS
 
 echo "Hello Ruhon deb"
